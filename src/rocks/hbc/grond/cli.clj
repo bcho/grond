7e1b4c38746dc9cfa8d4cc03aed3ba6329d95c26
hbc <bcxxxxxx@gmail.com>
2016-10-18 16:30:33 +0800

    feat: implement sub commands support

--- src/rocks/hbc/grond/cli.clj
+++ src/rocks/hbc/grond/cli.clj
@@ -2,21 +2,36 @@
   (:require [clojure.tools.cli :as cli])
   (:require [rocks.hbc.grond.protocol :as p]))
 
-(defrecord CliCommand [cmd-name cmd-options execute-fn]
+(def normalize-cmd-name (comp keyword name))
+
+(defrecord CliCommand [cmd-name execute-fn]
   p/Command
   (->name [_] cmd-name)
   (execute! [_ ctx args]
-    (execute-fn ctx (cli/parse-opts args cmd-options))))
+    (execute-fn ctx args)))
 
 (defn- execute-with-subcmds
   [sub-cmds]
-  (fn [ctx parsed-options]
-    (prn sub-cmds ctx parsed-options)
-    nil))
+  `(fn [ctx# args#]
+     (loop [args# args#]
+       (when-not (empty? args#)
+         (let [arg# (normalize-cmd-name (first args#))
+               args# (rest args#)]
+           (if-let [cmd# (some #(do (when (= (p/->name %) arg#) %)) ~sub-cmds)]
+             (p/execute! cmd# ctx# args#)
+             (recur args#)))))))
+
+(defn- execute-with-fn
+  [cmd-options fn-args fn-body]
+  `(fn [ctx# args#]
+     (prn args#)
+     (let [~(first fn-args) ctx#
+           ~(second fn-args) (cli/parse-opts args# ~cmd-options)]
+       ~@fn-body)))
 
 (defn- parse-cmd-decl
   [cmd-symbol cmd-decl]
-  (loop [cmd-name (keyword (name cmd-symbol))
+  (loop [cmd-name (normalize-cmd-name cmd-symbol)
          cmd-options []
          fn-args nil
          fn-body nil
@@ -27,29 +42,53 @@
         (assert cmd-name "`cmd-name` required")
         (assert cmd-options "`cmd-options` required")
         (if (some? sub-cmds)
-          [cmd-name cmd-options (execute-with-subcmds sub-cmds)]
-          [cmd-name cmd-options `(fn ~fn-args ~@fn-body)]))
+          [cmd-name (execute-with-subcmds sub-cmds)]
+          [cmd-name (execute-with-fn cmd-options fn-args fn-body)]))
       (let [arg (first args)
             args (rest args)]
         (cond
           (= :options arg)
-          (recur cmd-name (first args) fn-args fn-body sub-cmds (rest args))
+          (recur
+            cmd-name
+            (first args)
+            fn-args
+            fn-body
+            sub-cmds
+            (rest args))
 
           (= :name arg)
-          (recur (first args) cmd-options fn-args fn-body sub-cmds (rest args))
+          (recur
+            (normalize-cmd-name (first args))
+            cmd-options
+            fn-args
+            fn-body
+            sub-cmds
+            (rest args))
 
           (= :commands arg)
-          (recur cmd-name cmd-options fn-args fn-body (first args) (rest args))
+          (recur
+            cmd-name
+            cmd-options
+            fn-args
+            fn-body
+            (first args)
+            (rest args))
 
           :default
-          (recur cmd-name cmd-options arg args sub-cmds '()))))))
+          (recur
+            cmd-name
+            cmd-options
+            arg
+            args
+            sub-cmds
+            '()))))))
 
 (defmacro defcommand
   [cmd-symbol & cmd-decl]
-  (let [[cmd-name cmd-options execute-fn]
+  (let [[cmd-name execute-fn]
         (parse-cmd-decl cmd-symbol cmd-decl)]
     `(def ~cmd-symbol
-       (CliCommand. ~cmd-name ~cmd-options ~execute-fn))))
+       (CliCommand. ~cmd-name ~execute-fn))))
 
 (defn execute!
   [^CliCommand cmd args]
